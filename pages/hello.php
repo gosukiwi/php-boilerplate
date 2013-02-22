f6aca6158d94785a772d72c0ff19876668ddf020
Federico Ramírez <fedra.arg@gmail.com>
2013-02-22 11:01:53 -0300

    Added example pages

---
+++ pages/hello.php
@@ -0,0 +1,1 @@
+<h1>Hello, <?php echo _get(0);  ?></h1>
---
+++ pages/home.php
@@ -0,0 +1,6 @@
+<h1>Hello, World!</h1>
+
+<p>From PHP Boilerplate</p>
+
+<p>You can check out <?php echo $html->link('this', 'hello/world'); ?> page
+to see the example route working</p>
