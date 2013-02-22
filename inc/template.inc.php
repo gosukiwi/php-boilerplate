5ca6468e8dafe6054ac7d24337faeeed4d613275
Federico Ramírez <fedra.arg@gmail.com>
2013-02-22 10:59:47 -0300

    404 errors now always redirect nicely

--- inc/template.inc.php
+++ inc/template.inc.php
@@ -2,7 +2,9 @@
 if(!defined('INCLUDED')) exit('This file cannot be opened directly');
 
 // Returns the content of a page, used for templating
-function template_content() {
-    if(!route_match())
-        header('Location:404.html');
+function template_content() {    
+    if(!route_match()) {
+        global $html;
+        header('Location:' . $html->path_content('404.html'));
+    }
 }
