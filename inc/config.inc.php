f3c95e60cd37f098678e47f09b6761a4be9ce1bf
Federico Ramirez <fedra.arg@gmail.com>
2013-02-25 12:29:21 -0300

    Improve config.php comments

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -7,22 +7,16 @@ $config['site_template'] = 'default.tpl.php';
 // The title of your site
 $config['site_title'] = 'My App Name';
 
-// Your email address, in case of some site errores, this email will be shown
-// so users can contact you
-$config['contact_mail'] = 'admin@my-website.com';
-
 // Pretty urls will be in the format /index.php/pretty/url
 // If you have a .htaccess which rewrites everything paste /index.php/ to
 // the site root, disable this
 $config['fake_pretty_urls'] = true;
 
-// This is your timezone, it's not really used so leaving this by default will
-// do just fine, if you really want to update it, any of these values is valid:
-// <http://www.php.net/manual/en/timezones.php>
+// This is your timezone, it's used by PHP in all time related calculations
+// just leave this to the timezone you want the server to be in
 $config['timezone'] = 'America/Los_Angeles';
 
-// This is the duration of your session when you log-in, it's specified
-// in seconds.
+// The duration of PHP's $_SESSION variable
 $config['session_duration'] = 60 * 60 * 24 * 7; // One week
 
 // This is the absolute path to your app, if it's in the root path, just use '/'
