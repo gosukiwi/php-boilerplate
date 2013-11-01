2e5c1aa701e68245035b4da50ca4b19586e827a4
Federico Ramírez <fedekiler@gmail.com>
2013-10-31 23:35:48 -0300

    Welcome page is now a bit prettier

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,13 @@
-<h1>Hello, World!</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Congratulations!</h1> 
 
-<p>From PHP Boilerplate</p>
+            <p>php-boilerplate is up and running.</p>            
 
-<p>You can check out <?php echo $html->link('this', 'hello/world'); ?> page
-to see the example route working</p>
+            <p>You can check out <?php echo $html->link('this', 'hello/world'); ?> 
+            page to see the example route working</p>
+        </div>
+    </div>
+</div>
+
