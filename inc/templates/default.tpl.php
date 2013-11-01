9c7833d977701ad8e1fb41fc9d0c8f62dfed9d0d
Federico Ramírez <fedekiler@gmail.com>
2013-10-31 23:28:54 -0300

    Update bootstrap and jquery versions

--- inc/templates/default.tpl.php
+++ inc/templates/default.tpl.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <?php echo $html->css('css/bootstrap.min.css'); ?>
+    <?php echo $html->css('css/bootstrap-theme.min.css'); ?>
     <?php echo $html->css('css/app.css'); ?>
   </head>
   <body>
@@ -14,7 +15,7 @@
     <!-- This is the content placeholder, pages will be included here -->
     <?php echo template_content(); ?>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <?php echo $html->js('js/bootstrap.min.js'); ?>
     <?php echo $html->js('js/app.js'); ?>
   </body>
